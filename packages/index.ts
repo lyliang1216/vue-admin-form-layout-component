745039b3b4c25cac7899e4e09c7f805167506169
刘银亮 <lyliang@liuyinliangdeMacBook-Pro.local>
2023-07-15 17:43:15 +0800

    chore: 修改全局引入配置

--- packages/index.ts
+++ packages/index.ts
@@ -4,8 +4,4 @@ const install = function (app: any, name?: string) {
   app.component(name || FormLayout.name, FormLayout);
 };
 
-export default function (app: any) {
-  app.use(install);
-}
-
-export { FormLayout };
+export default { install, FormLayout };
